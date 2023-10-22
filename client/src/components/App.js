e70e7e1a6ee8fc12510cb29ff3ca715e75b412a8
Jacob-ochoa-02 <80126810+Jacob-ochoa-02@users.noreply.github.com>
2023-10-22 15:38:53 -0500

    Create App.js

---
+++ client/src/components/App.js
@@ -0,0 +1,22 @@
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+    
+//   );
+// }
+
+// well no way
+
+// window.addEventListener('scroll', ()=> {
+//   var differenceY = window.scrollY
+//   var divOf = document.querySelector('.Header-of');
+//   if(differenceY > 50) {
+//     divOf.classList.add('solid');
+//   }else {
+//     divOf.classList.remove('solid');
+//   }
+// });
+
+// export default App;
