26e8e38fd60afcdd999cf670b7c7f73b3cd80245
Jacob-ochoa-02 <80126810+Jacob-ochoa-02@users.noreply.github.com>
2023-11-27 07:11:16 -0500

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path='/*' element={<App/>}/>
+        </Routes>
+    </BrowserRouter>
+);
+
+reportWebVitals();
